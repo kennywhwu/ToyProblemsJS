519f1b03aa3b90d4b9419986394b663d9de28524
Jason Chambers <jas.o.chambers@gmail.com>
2017-01-29 22:24:47 -0600

    Added prompt 2 / prompt 2 answers

---
+++ Answers/Prompt2 Answers.js
@@ -0,0 +1,57 @@
+// You're having friends over for a super bowl party who have very 'specific' tastes.
+// Bradley is allergic to peanuts, and Harold doesn't like to eat anything that has
+// meat in it.
+
+// Given this info, return all the recipe names in an array that you can cook for your party.
+// Include the total cost as the last index of the array
+
+var recipes = [{
+  recipe: 'Pizza',
+  hasNuts: false,
+  hasMeat: true,
+  cost: 20
+}, {
+  recipe: 'Cheeseburgers',
+  hasNuts: false,
+  hasMeat: true,
+  cost: 7
+}, {
+  recipe: 'Salad',
+  hasNuts: true,
+  hasMeat: false,
+  cost: 15
+}, {
+  recipe: 'Chips and Dip',
+  hasNuts: false,
+  hasMeat: false,
+  cost: 8
+}, {
+  recipe: 'Alfredo',
+  hasNuts: true,
+  hasMeat: true,
+  cost: 15
+}, {
+  recipe: 'Cardboard',
+  hasNuts: false,
+  hasMeat: false,
+  cost: 3
+}];
+
+
+function getRecipes(arr) {
+  var cost = 0;
+
+  var acceptedRecipes = arr.reduce(function(acc, element) {
+    if (!element.hasNuts && !element.hasMeat) {
+      cost += element.cost;
+      acc.push(element.recipe);
+    }
+    return acc;
+  }, []);
+
+  acceptedRecipes.push('Total cost is ' + cost + ' dollars.');
+  return acceptedRecipes;
+}
+
+
+getRecipes(recipes);
---
+++ Prompts/Prompt2.js
@@ -0,0 +1,44 @@
+// You're having friends over for a super bowl party who have very 'specific' tastes.
+// Bradley is allergic to peanuts, and Harold doesn't like to eat anything that has
+// meat in it.
+
+// Given this info, return all the recipe names in an array that you can cook for your party.
+// Include the total cost as the last index of the array ex. 'Total cost is x dollars'
+
+var recipes = [{
+  recipe: 'Pizza',
+  hasNuts: false,
+  hasMeat: true,
+  cost: 20
+}, {
+  recipe: 'Cheeseburgers',
+  hasNuts: false,
+  hasMeat: true,
+  cost: 7
+}, {
+  recipe: 'Salad',
+  hasNuts: true,
+  hasMeat: false,
+  cost: 15
+}, {
+  recipe: 'Chips and Dip',
+  hasNuts: false,
+  hasMeat: false,
+  cost: 8
+}, {
+  recipe: 'Alfredo',
+  hasNuts: true,
+  hasMeat: true,
+  cost: 15
+}, {
+  recipe: 'Cardboard',
+  hasNuts: false,
+  hasMeat: false,
+  cost: 3
+}];
+
+
+function getRecipes(arr) {}
+
+
+// getRecipes(recipes);
