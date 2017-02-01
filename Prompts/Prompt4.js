b3079d3557ee58df1e4b7d3ceb483270469b1691
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-01 06:17:39 -0600

    Added prompt 4

---
+++ Prompts/Prompt4.js
@@ -0,0 +1,7 @@
+// Given a string, collect a letter count for all letters in the world, then only return the letters that appear more than once. Be sure to reuturn in the form of an object.
+
+// String will only contain lowercase letters from alphabet for simplicity
+
+// ex. letterCount('testing') === {'t': 2}
+
+function letterCount() {}
