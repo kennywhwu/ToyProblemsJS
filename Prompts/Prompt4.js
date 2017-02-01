6310956898600fdb86edc409eb7e6868af405164
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-01 06:19:23 -0600

    Update Prompt4.js
    'world' to 'word' in prompt description

--- Prompts/Prompt4.js
+++ Prompts/Prompt4.js
@@ -1,4 +1,4 @@
-// Given a string, collect a letter count for all letters in the world, then only return the letters that appear more than once. Be sure to reuturn in the form of an object.
+// Given a string, collect a letter count for all letters in the word, then only return the letters that appear more than once. Be sure to reuturn in the form of an object.
 
 // String will only contain lowercase letters from alphabet for simplicity
 
