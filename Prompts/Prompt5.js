726a51bb733cf73d6eb52d7e40d7e1c6e8907560
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-01 20:32:20 -0600

    Added prompt 5

---
+++ Prompts/Prompt5.js
@@ -0,0 +1,26 @@
+var buds = [{
+	name: 'bob',
+  broStatus: true
+}, {
+	name: 'joe',
+  broStatus: false
+}, {
+	name: 'emcee David James',
+  broStatus: true
+}, {
+	name: 'jimmi',
+  broStatus: true
+}, {
+	name: 'smitts',
+  broStatus: false
+}];
+
+// For each bud, check to see if they are a bro. If they are a bro, send them to an array with other bros.
+// Make use of at least one of the following methods(reduce, map, forEach, filter).
+
+function whoseABro(people) {}
+
+// extra credit: some of your buds are better than others. 
+// From time to time you need to update your list. Write a function that takes a bud, and updates the bro status.
+
+function swipeLeft(people, removedPerson) {}
