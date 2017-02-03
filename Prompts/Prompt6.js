91828e18945003f2fcf83f237441db7c25897132
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-03 10:34:42 -0800

    Added prompt 6

---
+++ Prompts/Prompt6.js
@@ -0,0 +1,15 @@
+// Check if the passed in movie is checked out of RedBox. If it is available, check out the movie, if it is not available, 
+// return a string saying the movie is already checked out.
+
+var movies = [{
+	title: 'Animal House',
+		checkedOut: false
+	},
+	{ title: 'The Avengers',
+	checkedOut: true
+	},
+	{ title: 'Lassie',
+	checkedOut: false
+	}];
+
+	function checkoutMovie() {}
