e125eef2537a7546881c536c4bb628a25703abe3
Jason Chambers <jas.o.chambers@gmail.com>
2017-05-19 13:53:46 -0700

    (solved) Average Pop. of Each Continent Hackerrank 1/1

---
+++ HackerRank/SQL/Average Population of Each Continent.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent
+
+SELECT country.continent, FLOOR(AVG(city.population)) 
+FROM city
+INNER JOIN country ON city.countrycode = country.code
+GROUP BY country.continent;
