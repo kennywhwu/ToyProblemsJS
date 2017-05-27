ebf383e9a01698929b24625326a99e24e495c45d
Jason Chambers <jas.o.chambers@gmail.com>
2017-05-27 15:15:58 -0700

    add prompt8

---
+++ Self Made/Prompts/Prompt8.js
@@ -0,0 +1,5 @@
+// write a function that takes two arguments (arr, num) and returns an array whose values are multiplied by num, do this using recursion
+// ex ([1, 2, 3, 4, 5] num = 4) => [4, 8, 12, 16, 20]
+
+const recursiveMultiplier = (arr, num) => {};
+
