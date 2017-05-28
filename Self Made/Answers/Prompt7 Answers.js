d377271931e948dccdccbcf666263f19677d07e5
Jason Chambers <jas.o.chambers@gmail.com>
2017-05-27 21:14:58 -0700

    (fix) fixed so no longer mutating input data in p7 and p8

--- Self Made/Answers/Prompt7 Answers.js
+++ Self Made/Answers/Prompt7 Answers.js
@@ -1,12 +1,12 @@
 // write a function using recursion that takes an array and returns an array that is the reversed version
 // ex [1, 2, 3, 4, 5] => [5, 4, 3, 2, 1]
 
-const reverseArray = (arr, reversed = []) => {
-  if (!arr.length) {
+const reverseArray = (arr, reversed = [], i = arr.length-1) => {
+  if (i < 0) {
     return reversed;
   }
-  reversed.push(arr.pop());
-  return reverseArray(arr, reversed);
+  reversed.push(arr[i]);
+  return reverseArray(arr, reversed, i-1);
 };
 
 console.log(reverseArray([1, 2, 3, 4, 5]))
--- Self Made/Answers/Prompt8 Answers.js
+++ Self Made/Answers/Prompt8 Answers.js
@@ -1,12 +1,12 @@
 // write a function that takes two arguments (arr, num) and returns an array whose values are multiplied by num, do this using recursion
 // ex ([1, 2, 3, 4, 5] num = 4) => [4, 8, 12, 16, 20]
 
-const recursiveMultiplier = (arr, num, n = 0) => {
+const recursiveMultiplier = (arr, num, r = [], n = 0) => {
   if (n === arr.length) {
-    return arr;
+    return r;
   }
-  arr[n] *= num;
-  return recursiveMultiplier(arr, num, n+1);
+  r[n] = arr[n] * num;
+  return recursiveMultiplier(arr, num, r, n+1);
 };
 
 console.log(recursiveMultiplier([1, 2, 3, 4, 5], 4));
